948ab2b2f2b5ed128ca7fb399328d333b67bb1fc
zominym <marc.zominy@etu.univ-lyon1.fr>
2017-01-03 09:12:37 +0100

    created main

---
+++ src/nego/Main.java
@@ -0,0 +1,11 @@
+package nego;
+
+/**
+ * Created by atanakar on 03/01/17.
+ */
+public class Main {
+
+    public static void main (String args[] ) {
+        System.out.println("Mabit");
+    }
+}
